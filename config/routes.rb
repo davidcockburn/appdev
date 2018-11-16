203cbc9cd6fcaf509e225877b7e09edb8b792ba0
dc <dc@davidcockburn.com>
2018-11-16 10:18:33 -0500

    Devise generators complete for User model with all options active

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  root to: "subcontractor#index"
+  
   resources :subcontractors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
